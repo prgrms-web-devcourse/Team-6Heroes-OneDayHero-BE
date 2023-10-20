e04059707cf4f335e1db5c6017f7e8a0f632e849
Bombo <bombo96@chungwoon.ac.kr>
2023-10-20 23:35:14 +0900

    [SIX-11] feat : 미션 매칭 도메인 구현

---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/missionmatch/MissionMatch.java
@@ -0,0 +1,27 @@
+package com.sixheroes.onedayherodomain.missionmatch;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "mission_matches")
+@Entity
+public class MissionMatch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "mission_id", nullable = false)
+    private Long missionId;
+
+    @Column(name = "hero_id", nullable = false)
+    private Long heroId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private MissionMatchStatus missionMatchStatus;
+}
---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/missionmatch/MissionMatchStatus.java
@@ -0,0 +1,4 @@
+package com.sixheroes.onedayherodomain.missionmatch;
+
+public enum MissionMatchStatus {
+}
