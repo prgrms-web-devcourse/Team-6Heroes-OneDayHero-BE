71118ce41b5466c6f7577b85035a7bbbc716d0c8
Bombo <bombo96@chungwoon.ac.kr>
2023-10-20 17:55:51 +0900

    fix : Spring Bean이 테스트에서 전체 패키지에 따라 자동주입되도록 scanBasePackages 추가

--- onedayhero-application/src/test/java/com/sixheroes/onedayheroapplication/OnedayheroApplicationTests.java
+++ onedayhero-application/src/test/java/com/sixheroes/onedayheroapplication/OnedayheroApplicationTests.java
@@ -1,9 +1,10 @@
 package com.sixheroes.onedayheroapplication;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootApplication(scanBasePackages = "com.sixheroes")
 class OnedayheroApplicationTests {
 
     @Test
