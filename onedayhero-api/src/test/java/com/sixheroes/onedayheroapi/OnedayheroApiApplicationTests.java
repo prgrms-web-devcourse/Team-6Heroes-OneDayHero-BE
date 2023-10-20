874c15a181eea66844c1b289b81364270ee20f0e
PHJ <legowww@gmail.com>
2023-10-20 17:39:24 +0900

    init : 프로젝트 생성

---
+++ onedayhero-api/src/test/java/com/sixheroes/onedayheroapi/OnedayheroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixheroes.onedayheroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnedayheroApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ onedayhero-common/src/test/java/com/sixheroes/onedayherocommon/OnedayheroCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixheroes.onedayherocommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnedayheroCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
