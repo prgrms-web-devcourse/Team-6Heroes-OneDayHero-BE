33c07b0438ed63f979496321522a34c20c1b959a
Bombo <bombo96@chungwoon.ac.kr>
2023-10-22 16:29:47 +0900

    [SIX-4] feat : 유저 업적 도메인 추가

---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/achievement/UserAchievement.java
@@ -0,0 +1,23 @@
+package com.sixheroes.onedayherodomain.achievement;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user_achievements")
+@Entity
+public class UserAchievement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "achievement_id", nullable = false)
+    private Long achievementId;
+}
