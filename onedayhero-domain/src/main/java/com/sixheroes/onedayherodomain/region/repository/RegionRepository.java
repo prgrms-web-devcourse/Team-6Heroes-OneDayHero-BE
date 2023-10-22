64b0ac3e417adfdecae3b889afeba690c3810f1c
PHJ <legowww@gmail.com>
2023-10-22 22:29:56 +0900

    [SIX-9] feat : 지역 도메인 추가

---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/region/repository/RegionRepository.java
@@ -0,0 +1,7 @@
+package com.sixheroes.onedayherodomain.region.repository;
+
+import com.sixheroes.onedayherodomain.region.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, Long> {
+}
