edd3715cdab66d7fb3a9cd3c31922d51382c2c9e
PHJ <legowww@gmail.com>
2023-10-22 22:21:12 +0900

    [SIX-8] feat : 미션 챗 룸 도메인 추가

---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/missionchatroom/MissionChatRoom.java
@@ -0,0 +1,28 @@
+package com.sixheroes.onedayherodomain.missionchatroom;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "mission_chat_rooms")
+@Entity
+public class MissionChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "mission_id", nullable = false)
+    private Long missionId;
+
+    @Column(name = "hero_id", nullable = false)
+    private Long heroId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private MissionChatRoomStatus missionChatRoomStatus;
+}
