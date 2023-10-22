d8d69658fb0bac416b0c81b5bb4f83b1d00bc918
Bombo <bombo96@chungwoon.ac.kr>
2023-10-22 16:14:43 +0900

    [SIX-20] feat : 신고 카테고리 도메인 추가

---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/report/ReportCategory.java
@@ -0,0 +1,20 @@
+package com.sixheroes.onedayherodomain.report;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "report_category")
+@Entity
+public class ReportCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/report/repository/ReportCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.sixheroes.onedayherodomain.report.repository;
+
+import com.sixheroes.onedayherodomain.report.ReportCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportCategoryRepository extends JpaRepository<ReportCategory, Long> {
+}
