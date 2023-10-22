f9761f079c14bb30e895cadbabd143007b401662
Bombo <bombo96@chungwoon.ac.kr>
2023-10-22 16:22:50 +0900

    [SIX-13] 유저 미션 카테고리 도메인 추가

---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/usermission/repository/UserMissionCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.sixheroes.onedayherodomain.usermission.repository;
+
+import com.sixheroes.onedayherodomain.usermission.UserMissionCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserMissionCategoryRepository extends JpaRepository<UserMissionCategory, Long> {
+}
