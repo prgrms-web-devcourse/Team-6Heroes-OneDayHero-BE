8eb68ff6934b00a4724fbb7726eae5ffd6e6c8b1
Bombo <bombo96@chungwoon.ac.kr>
2023-10-22 16:25:11 +0900

    [SIX-10] feat : 유저 지역 도메인 추가

---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/region/repository/UserRegionRepository.java
@@ -0,0 +1,7 @@
+package com.sixheroes.onedayherodomain.region.repository;
+
+import com.sixheroes.onedayherodomain.region.UserRegion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRegionRepository extends JpaRepository<UserRegion, Long> {
+}
