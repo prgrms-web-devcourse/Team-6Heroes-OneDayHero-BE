21d7cd1d2863d4820a63f11cbb1534cbdb473e71
Bombo <bombo96@chungwoon.ac.kr>
2023-10-23 16:51:43 +0900

    [SIX-25] feat : 기본 AdviceHandler 포맷 구현

---
+++ onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/global/handler/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.sixheroes.onedayheroapi.global.handler;
+
+import com.sixheroes.onedayheroapi.global.response.ErrorCode;
+import com.sixheroes.onedayheroapi.global.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ErrorResponse handleException(Exception exception) {
+        log.error("Error occurred", exception);
+        var errorCode = ErrorCode.S_001;
+        return ErrorResponse.from(errorCode);
+    }
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public ErrorResponse handleRuntimeException(RuntimeException exception) {
+        var errorCode = ErrorCode.valueOf(exception.getMessage());
+        return ErrorResponse.from(errorCode);
+    }
+
+    @ExceptionHandler(value = BindException.class)
+    public ErrorResponse handleBindException(BindException exception) {
+        var errorCode = ErrorCode.T_001;
+        return ErrorResponse.from(errorCode, exception);
+    }
+}
