f1f10a1ab037adf9b56ac82ceeae160ac5777ccd
PHJ <legowww@gmail.com>
2023-10-24 17:34:58 +0900

    [SIX-33] feat : BaseEntity 및 관련 Configuration 구현

---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/global/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.sixheroes.onedayherodomain.global;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@ToString
+public abstract class BaseEntity {
+
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
