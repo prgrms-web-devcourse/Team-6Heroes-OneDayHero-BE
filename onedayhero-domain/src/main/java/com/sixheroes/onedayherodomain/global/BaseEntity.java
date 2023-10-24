f736c82260d28d2b13862910aee7fb197615cb8f
PHJ <legowww@gmail.com>
2023-10-24 18:14:08 +0900

    [SIX-33] feat : 누락된 nullable=false 속성값 추가

--- onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/global/BaseEntity.java
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/global/BaseEntity.java
@@ -17,11 +17,11 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public abstract class BaseEntity {
 
-    @Column(name = "created_at", updatable = false)
+    @Column(name = "created_at", nullable = false)
     @CreatedDate
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at")
+    @Column(name = "updated_at", nullable = false)
     @LastModifiedDate
     private LocalDateTime updatedAt;
 }
