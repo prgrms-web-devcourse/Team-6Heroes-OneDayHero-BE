c12a574e4d2361f1437b960707d22b4782c45238
heenahan <mby1997s@gmail.com>
2023-10-24 17:28:41 +0900

    [SIX-35] feat : 유저 미션 채팅방, 유저 미션 북마크, 미션 매칭 요청 도메인 구현 및 Repository 추가

---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/missionchatroom/repository/UserMissionChatRoomRepository.java
@@ -0,0 +1,7 @@
+package com.sixheroes.onedayherodomain.missionchatroom.repository;
+
+import com.sixheroes.onedayherodomain.missionchatroom.UserMissionChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserMissionChatRoomRepository extends JpaRepository<UserMissionChatRoom, Long> {
+}
