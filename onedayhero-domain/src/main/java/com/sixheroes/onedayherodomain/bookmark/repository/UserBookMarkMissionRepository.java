e85b7135590a6b3e7812ee95b1942d6e3b424781
heenahan <mby1997s@gmail.com>
2023-10-24 17:28:41 +0900

    [SIX-35] feat : 유저 미션 채팅방, 유저 미션 북마크, 미션 매칭 요청 도메인 구현 및 Repository 추가

---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/bookmark/repository/UserBookMarkMissionRepository.java
@@ -0,0 +1,7 @@
+package com.sixheroes.onedayherodomain.bookmark.repository;
+
+import com.sixheroes.onedayherodomain.bookmark.UserBookMarkMission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserBookMarkMissionRepository extends JpaRepository<UserBookMarkMission, Long> {
+}
