7bd166f2b863d8ecb004da555d6ac4eff3d1da96
Bombo <bombo96@chungwoon.ac.kr>
2023-10-25 20:53:51 +0900

    [SIX-47] feat : Mission 생성에 대한 누락된 검증 로직을 추가

--- onedayhero-application/src/main/java/com/sixheroes/onedayheroapplication/mission/MissionService.java
+++ onedayhero-application/src/main/java/com/sixheroes/onedayheroapplication/mission/MissionService.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.NoSuchElementException;
 
 @Slf4j
@@ -29,6 +30,8 @@ public class MissionService {
                 });
 
         var mission = request.toEntity(missionCategory);
+        mission.validRangeOfMissionTime(LocalDateTime.now());
+
         var savedMission = missionRepository.save(mission);
 
         return new MissionResponse(savedMission);
