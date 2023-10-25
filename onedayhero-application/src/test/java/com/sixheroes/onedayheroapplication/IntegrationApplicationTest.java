adc74993e339c36286a97be92cf8b608286a2a02
Bombo <bombo96@chungwoon.ac.kr>
2023-10-25 13:26:00 +0900

    [SIX-50] feat : basePackage 변경으로 인한 scanBase 제거 및 통합 테스트 클래스 추가

---
+++ onedayhero-application/src/test/java/com/sixheroes/onedayheroapplication/IntegrationApplicationTest.java
@@ -0,0 +1,8 @@
+package com.sixheroes.onedayheroapplication;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public abstract class IntegrationApplicationTest {
+
+}
