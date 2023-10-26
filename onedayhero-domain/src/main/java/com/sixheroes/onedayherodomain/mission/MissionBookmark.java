415aae0e61b2abe0e00ec88d3885e1cbb149312d
PHJ <legowww@gmail.com>
2023-10-26 15:23:51 +0900

    [SIX-63] feat : 찜 도메인 코드 작성

---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/mission/MissionBookmark.java
@@ -0,0 +1,35 @@
+package com.sixheroes.onedayherodomain.mission;
+
+import com.sixheroes.onedayherodomain.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "mission_bookmarks")
+@Entity
+public class MissionBookmark extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id", nullable = false)
+    private Mission mission;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Builder
+    private MissionBookmark(
+            Mission mission,
+            Long userId
+    ) {
+        this.mission = mission;
+        this.userId = userId;
+    }
+}
---
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/mission/repository/MissionBookmarkRepository.java
@@ -0,0 +1,7 @@
+package com.sixheroes.onedayherodomain.mission.repository;
+
+import com.sixheroes.onedayherodomain.mission.MissionBookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionBookmarkRepository extends JpaRepository<MissionBookmark, Long> {
+}
