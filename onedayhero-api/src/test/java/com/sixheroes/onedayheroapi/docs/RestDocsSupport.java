450e8a4b9ae622b2ac40ecd413f32b60a6bcba74
Bombo <bombo96@chungwoon.ac.kr>
2023-10-26 12:39:36 +0900

    feat : Restdocs 관련 설정을 추가

---
+++ onedayhero-api/src/test/java/com/sixheroes/onedayheroapi/docs/RestDocsSupport.java
@@ -0,0 +1,35 @@
+package com.sixheroes.onedayheroapi.docs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sixheroes.onedayheroapi.global.handler.GlobalExceptionHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class RestDocsSupport {
+
+    protected MockMvc mockMvc;
+    protected ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setup(RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(setController())
+                .setControllerAdvice(new GlobalExceptionHandler())
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .apply(MockMvcRestDocumentation.documentationConfiguration(provider)
+                        .operationPreprocessors()
+                        .withRequestDefaults(prettyPrint())
+                        .withResponseDefaults(prettyPrint()))
+                .build();
+    }
+
+    protected abstract Object setController();
+}
