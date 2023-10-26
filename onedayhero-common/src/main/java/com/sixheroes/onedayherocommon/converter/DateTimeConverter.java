deb4b38a6ee705c928459b74d789eb408f0abe14
Bombo <bombo96@chungwoon.ac.kr>
2023-10-26 23:49:05 +0900

    [SIX-49] test : api 검증에 시간 값 검증을 위한 Converter를 추가하고 필수 값 검증 테스트 삭제

--- onedayhero-common/src/main/java/com/sixheroes/onedayherocommon/converter/DateTimeConverter.java
+++ onedayhero-common/src/main/java/com/sixheroes/onedayherocommon/converter/DateTimeConverter.java
@@ -1,11 +1,16 @@
 package com.sixheroes.onedayherocommon.converter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public final class DateTimeConverter {
 
+    public static String convertLocalDateTimeToString(LocalDateTime localDateTime) {
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
+    }
+    
     public static String convertDateToString(LocalDate localDate) {
         return localDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
