182fcfdd394a59a986a1f42e50d4bb73a93b2742
heenahan <mby1997s@gmail.com>
2023-10-27 00:31:34 +0900

    [SIX-57] feat : 유저 정보 수정 및 필드 검증에 대한 도메인 로직 구현 및 테스트

--- onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/user/Week.java
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/user/Week.java
@@ -1,7 +1,11 @@
 package com.sixheroes.onedayherodomain.user;
 
+import com.sixheroes.onedayherocommon.error.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
 
 @Getter
 @RequiredArgsConstructor
@@ -15,4 +19,11 @@ public enum Week {
     SUN("일요일");
 
     private final String description;
+
+    public static Week from(String week) {
+        return Arrays.stream(Week.values())
+            .filter(w -> week.equals(w.name()))
+            .findFirst()
+            .orElseThrow(() -> new NoSuchElementException(ErrorCode.EU_008.name()));
+    }
 }
