c3296c43e73ef6ee303bd15ec4d5069a8bc59faf
PHJ <legowww@gmail.com>
2023-10-27 17:06:28 +0900

    [SIX-67] feat : API 요청 DTO NULL 검증 구현

--- onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/mission/request/MissionBookmarkCreateRequest.java
+++ onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/mission/request/MissionBookmarkCreateRequest.java
@@ -1,9 +1,13 @@
 package com.sixheroes.onedayheroapi.mission.request;
 
 import com.sixheroes.onedayheroapplication.mission.request.MissionBookmarkCreateServiceRequest;
+import jakarta.validation.constraints.NotNull;
 
 public record MissionBookmarkCreateRequest(
+        @NotNull(message = "미션 아이디는 필수 값 입니다.")
         Long missionId,
+
+        @NotNull(message = "유저 아이디는 필수 값 입니다.")
         Long userId
 ) {
 
