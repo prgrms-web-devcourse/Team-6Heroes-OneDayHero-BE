bac4f0047b491c57f2a56c56a7c792d27aa971e9
heenahan <mby1997s@gmail.com>
2023-10-28 10:40:33 +0900

    [SIX-69] refactor : validation message 수정 & service request 변경에 따른 controller request 변경

--- onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/user/request/UserUpadateRequest.java
+++ onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/user/request/UserUpadateRequest.java
@@ -1,11 +1,13 @@
 package com.sixheroes.onedayheroapi.user.request;
 
+import com.sixheroes.onedayheroapplication.user.dto.UserBasicInfoServiceDto;
+import com.sixheroes.onedayheroapplication.user.dto.UserFavoriteWorkingDayServiceDto;
 import com.sixheroes.onedayheroapplication.user.request.UserServiceUpdateRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public record UserUpadateRequest(
-    @NotNull(message = "유저 아이디가 null입니다.")
+    @NotNull(message = "유저 아이디는 필수값 입니다.")
     Long userId,
 
     @Valid
@@ -14,15 +16,23 @@ public record UserUpadateRequest(
     UserFavoriteWorkingDayDto favoriteWorkingDay
 ) {
     public UserServiceUpdateRequest toService() {
-        return UserServiceUpdateRequest.from(
-            userId,
-            basicInfo.nickname(),
-            basicInfo.gender(),
-            basicInfo.birth(),
-            basicInfo.introduce(),
-            favoriteWorkingDay.favoriteDate(),
-            favoriteWorkingDay.favoriteStartTime(),
-            favoriteWorkingDay.favoriteEndTime()
-        );
+        var userBasicInfoServiceDto = UserBasicInfoServiceDto.builder()
+            .nickname(basicInfo.nickname())
+            .gender(basicInfo.gender())
+            .birth(basicInfo.birth())
+            .introduce(basicInfo.introduce())
+            .build();
+
+        var userFavoriteWorkingDayServiceDto = UserFavoriteWorkingDayServiceDto.builder()
+            .favoriteDate(favoriteWorkingDay.favoriteDate())
+            .favoriteStartTime(favoriteWorkingDay.favoriteStartTime())
+            .favoriteEndTime(favoriteWorkingDay.favoriteEndTime())
+            .build();
+
+        return UserServiceUpdateRequest.builder()
+            .userId(userId)
+            .userBasicInfo(userBasicInfoServiceDto)
+            .userFavoriteWorkingDay(userFavoriteWorkingDayServiceDto)
+            .build();
     }
 }
