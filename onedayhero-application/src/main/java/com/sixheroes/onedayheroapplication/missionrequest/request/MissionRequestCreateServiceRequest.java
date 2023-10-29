0b0d58845dae2d11f475c7016019f53ab0092a93
heenahan <mby1997s@gmail.com>
2023-10-29 22:54:46 +0900

    [SIX-99] feat & test : 미션 제안 생성에 대한 서비스 로직 구현 및 테스트

--- onedayhero-application/src/main/java/com/sixheroes/onedayheroapplication/missionrequest/request/MissionRequestCreateServiceRequest.java
+++ onedayhero-application/src/main/java/com/sixheroes/onedayheroapplication/missionrequest/request/MissionRequestCreateServiceRequest.java
@@ -1,10 +1,18 @@
 package com.sixheroes.onedayheroapplication.missionrequest.request;
 
+import com.sixheroes.onedayherodomain.missionrequest.MissionRequest;
 import lombok.Builder;
 
 @Builder
 public record MissionRequestCreateServiceRequest(
+    Long userId,
     Long missionId,
     Long heroId
 ) {
+    public MissionRequest toEntity() {
+        return MissionRequest.builder()
+            .missionId(missionId)
+            .heroId(heroId)
+            .build();
+    }
 }
