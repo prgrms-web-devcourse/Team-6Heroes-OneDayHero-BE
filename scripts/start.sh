6855b8540fe3cf3e9b454f8f14356f3090d56aa2
JongWoon Moon <74203371+bombo-dev@users.noreply.github.com>
2023-10-31 13:59:26 +0900

    [SIX-93] feat : NGINX 무중단 배포 구현
    * docs : pr 올릴 시 test 자동 수행 action 추가
    
    * docs : PR 템플릿 Checklist 추가
    
    * [SIX-93] docs : 개발 서버 배포 파일을 S3에 전송
    
    * [SIX-93] docs : 개발 서버 배포 파일을 S3에 전송
    
    * [SIX-93] docs : 개발 서버 배포 파일을 S3에 전송하는 CI 수정
    
    * [SIX-93] docs : 무중단 배포 scripts 및 docker 추가
    
    * [SIX-93] docs : GITHUB_ACITON GROUP_NAME 수정
    
    * [SIX-93] fix : plain.jar 가 생기지 않도록 수정
    
    * [SIX-93] fix : healthCheck api 구현
    
    * [SIX-93] feat : restdocs 요청 url 추가
    
    * [SIX-93] fix : docker-compose 수정
    
    * [SIX-93] docs : 변경된 무중단 배포 CD 스크립트 적용
    
    * [SIX-93] fix : RESTDOCS 호출 api 추가
    
    * [SIX-93] feat : RESTDOCS static 자원을 읽기 위한 설정파일 추가
    
    * [SIX-93] feat : 정적 파일을 호출하는 메서드 추가
    
    * [SIX-93] fix : restdocs를 호출하는 로직 제거
    
    ---------
    
    Co-authored-by: heenahan <mby1997s@gmail.com>

---
+++ scripts/start.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+IDLE_PORT=$(find_idle_port)
+REPOSITORY=/home/ec2-user/onedayhero/
+LOCATION=/yml/application.yml
+NGINX_NAME=nginx
+
+echo "> Build 파일 복사"
+echo "> cp $REPOSITORY*.jar $REPOSITORY"
+
+cp $REPOSITORY/*.jar $REPOSITORY      # 새로운 jar file 계속 덮어쓰기
+
+echo "> 새 어플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+
+if [ "$(docker ps -q -f name=$NGINX_NAME)" ]; then
+  echo "Docker 컨테이너 '$NGINX_NAME'가 이미 실행 중입니다."
+else
+  # Docker 컨테이너 실행
+  echo "> docker run -it --name nginx -d -v /etc/nginx/:/etc/nginx/ -p 80:80 nginx"
+  # sudo docker run -it --name nginx -d -v /etc/nginx/:/etc/nginx/ -p 80:80 nginx
+  echo "> docker start nginx"
+  sudo docker start nginx
+  echo "Docker 컨테이너 '$NGINX_NAME'가 실행되었습니다."
+  sleep 3
+fi
+
+
+cd $REPOSITORY
+
+files=$(ls)
+echo "$files"
+
+# docker 이미지를 연결하고 환경변수 전달
+echo "> docker build -t spring ./"
+sudo docker build -t spring ./
+echo "> docker run -it --name ${IDLE_PROFILE} -d -e active=${IDLE_PROFILE} -e location=${LOCATION} -p ${IDLE_PORT}:${IDLE_PORT} spring"
+sudo docker run -it --name "$IDLE_PROFILE" -d -e active="$IDLE_PROFILE" -e location=$LOCATION -v /home/ec2-user/yml/application.yml:/yml/application.yml -p "$IDLE_PORT":"$IDLE_PORT" spring
