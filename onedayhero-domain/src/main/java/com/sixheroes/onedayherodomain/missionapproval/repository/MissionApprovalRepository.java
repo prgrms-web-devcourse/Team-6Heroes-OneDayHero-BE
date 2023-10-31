5dd7ab292ce3588c85e02ca5280bc56fe53f190b
Bombo <bombo96@chungwoon.ac.kr>
2023-10-31 15:49:39 +0900

    [SIX-117] refactor : 미션 승인 엔티티 삭제

--- onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/missionapproval/repository/MissionApprovalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sixheroes.onedayherodomain.missionapproval.repository;
-
-import com.sixheroes.onedayherodomain.missionapproval.MissionApproval;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MissionApprovalRepository extends JpaRepository<MissionApproval, Long> {
-}
