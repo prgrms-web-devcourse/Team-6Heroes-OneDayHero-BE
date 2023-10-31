2fba85ef9b25941b9cd813596b595e1a2fbe00e9
heenahan <mby1997s@gmail.com>
2023-10-31 16:45:57 +0900

    [SIX-121] feat : QueryDsl 설정 추가

---
+++ onedayhero-infra-querydsl/src/main/java/com/sixheroes/onedayheroinfraquerydsl/config/QueryDslConfiguration.java
@@ -0,0 +1,19 @@
+package com.sixheroes.onedayheroinfraquerydsl.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfiguration {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
+}
---
+++ onedayhero-infra-querydsl/src/test/java/com/sixheroes/onedayheroinfraquerydsl/IntegrationQueryDslTest.java
@@ -0,0 +1,7 @@
+package com.sixheroes.onedayheroinfraquerydsl;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public abstract class IntegrationQueryDslTest {
+}
