1dba7950e31f1807774932999fac607f0cd9ace9
Bombo <bombo96@chungwoon.ac.kr>
2023-11-01 14:35:17 +0900

    [SIX-122] feat : queryRepository 를 별도로 가지도록 변경

--- onedayhero-infra-querydsl/src/main/java/com/sixheroes/onedayheroquerydsl/mission/MissionQueryRepository.java
+++ onedayhero-infra-querydsl/src/main/java/com/sixheroes/onedayheroquerydsl/mission/MissionQueryRepository.java
@@ -14,16 +14,14 @@ import static com.sixheroes.onedayherodomain.mission.QMission.mission;
 
 @Repository
 @RequiredArgsConstructor
-public class MissionRepositoryCustomImpl implements MissionRepositoryCustom {
+public class MissionQueryRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    @Override
     public Slice<Mission> search(Long categoryId, Long regionId, LocalDate findDate) {
         return null;
     }
 
-    @Override
     public List<Mission> findByCategoryId(Long categoryId) {
         return queryFactory.select(mission)
                 .from(mission)
