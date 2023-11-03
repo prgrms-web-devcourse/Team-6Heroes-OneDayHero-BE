bb6c7e6eb8669f83091b65a1e9ca2c2b2304b9ba
heenahan <mby1997s@gmail.com>
2023-11-03 15:40:34 +0900

    [SIX-28] refactor : 조회 기능 Reader로 이동, NPE 방어적으로 작성, this 명시, 메서드 이름 변경

---
+++ onedayhero-application/src/main/java/com/sixheroes/onedayheroapplication/user/UserReader.java
@@ -0,0 +1,30 @@
+package com.sixheroes.onedayheroapplication.user;
+
+import com.sixheroes.onedayherocommon.error.ErrorCode;
+import com.sixheroes.onedayherodomain.user.User;
+import com.sixheroes.onedayherodomain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Component
+public class UserReader {
+
+    private final UserRepository userRepository;
+
+    public User findOne(
+            Long userId
+    ) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> {
+                    log.debug("존재하지 않는 유저 아이디입니다. userId : {}", userId);
+                    return new NoSuchElementException(ErrorCode.EUC_001.name());
+                });
+    }
+}
--- onedayhero-application/src/main/java/com/sixheroes/onedayheroapplication/user/UserService.java
+++ onedayhero-application/src/main/java/com/sixheroes/onedayheroapplication/user/UserService.java
@@ -2,30 +2,22 @@ package com.sixheroes.onedayheroapplication.user;
 
 import com.sixheroes.onedayheroapplication.user.request.UserServiceUpdateRequest;
 import com.sixheroes.onedayheroapplication.user.response.UserUpdateResponse;
-import com.sixheroes.onedayherocommon.error.ErrorCode;
-import com.sixheroes.onedayherodomain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.NoSuchElementException;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
+    private final UserReader userReader;
 
     @Transactional
     public UserUpdateResponse updateUser(UserServiceUpdateRequest userServiceUpdateRequest) {
         var userId = userServiceUpdateRequest.userId();
-        var user = userRepository.findById(userId)
-                            .orElseThrow(() -> {
-                                log.debug("존재하지 않는 유저 아이디입니다. userId : {}", userId);
-                                return new NoSuchElementException(ErrorCode.EUC_001.name());
-                            });
+        var user = userReader.findOne(userId);
 
         var userBasicInfo = userServiceUpdateRequest.toUserBasicInfo();
         var userFavoriteWorkingDay = userServiceUpdateRequest.toUserFavoriteWorkingDay();
