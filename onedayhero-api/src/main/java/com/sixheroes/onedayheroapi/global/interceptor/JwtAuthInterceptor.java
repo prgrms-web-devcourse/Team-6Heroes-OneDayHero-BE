f822e30f53f2ac92c746a4ee1d71ef3ed9ba2fbf
PHJ <legowww@gmail.com>
2023-11-03 23:04:43 +0900

    [SIX-133] feat : HttpServletRequest 을 id 임시 저장소로 사용하도록 변경

--- onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/global/interceptor/JwtAuthInterceptor.java
+++ onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/global/interceptor/JwtAuthInterceptor.java
@@ -1,6 +1,5 @@
 package com.sixheroes.onedayheroapi.global.interceptor;
 
-import com.sixheroes.onedayheroapi.global.jwt.AuthContextHolder;
 import com.sixheroes.onedayherocommon.error.ErrorCode;
 import com.sixheroes.onedayherocommon.jwt.JwtTokenUtils;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,12 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 
 @Slf4j
 public class JwtAuthInterceptor implements HandlerInterceptor {
 
+    private static final String USER_ID = "id";
     private final static String TEST_SECRET_KEY = "EENY5W0eegTf1naQB2eDeaaaRS2b8xa5c4qLdS0hmVjtbvo8tOyhPMcAmtPuQ";
 
     @Override
@@ -35,28 +34,19 @@ public class JwtAuthInterceptor implements HandlerInterceptor {
         }
 
         var id = JwtTokenUtils.getId(getAccessToken(authorizationHeader), TEST_SECRET_KEY);
-        AuthContextHolder.setContext(id);
+        request.setAttribute(USER_ID, id);
 
         return true;
     }
 
-    @Override
-    public void postHandle(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Object handler,
-            ModelAndView modelAndView
-    ) {
-        AuthContextHolder.clearContext();
-    }
-
-    private static boolean validateAuthorizationHeaderIsValid(String header) {
+    private boolean validateAuthorizationHeaderIsValid(String header) {
         return header == null || !header.startsWith("Bearer ");
     }
 
-    private static String getAuthorization(HttpServletRequest request) {
+    private String getAuthorization(HttpServletRequest request) {
         return request.getHeader(HttpHeaders.AUTHORIZATION);
     }
+
     private String getAccessToken(String header) {
         return header.split(" ")[1].trim();
     }
--- onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/global/jwt/AuthArgumentResolver.java
+++ onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/global/jwt/AuthArgumentResolver.java
@@ -1,12 +1,17 @@
 package com.sixheroes.onedayheroapi.global.jwt;
 
+import com.sixheroes.onedayherocommon.error.ErrorCode;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private static final String USER_ID = "id";
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -23,6 +28,13 @@ public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
             NativeWebRequest webRequest,
             WebDataBinderFactory binderFactory
     ) {
-        return AuthContextHolder.getContext();
+        var request = webRequest.getNativeRequest(HttpServletRequest.class);
+        var id = request.getAttribute(USER_ID);
+
+        if (ObjectUtils.isEmpty(id)) {
+            throw new IllegalStateException(ErrorCode.S_001.name());
+        }
+
+        return id;
     }
 }
