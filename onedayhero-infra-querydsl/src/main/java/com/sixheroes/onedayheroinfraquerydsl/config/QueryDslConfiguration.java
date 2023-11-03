e492d4332b7759bdea9f1f44b8dabdf8333a8f72
heenahan <mby1997s@gmail.com>
2023-11-03 13:18:18 +0900

    [SIX-82] feat & test : 제안받은 미션 페이징 조회 Repository 구현 및 테스트

--- onedayhero-infra-querydsl/src/main/java/com/sixheroes/onedayheroinfraquerydsl/config/QueryDslConfiguration.java
+++ onedayhero-infra-querydsl/src/main/java/com/sixheroes/onedayheroinfraquerydsl/config/QueryDslConfiguration.java
@@ -2,15 +2,15 @@ package com.sixheroes.onedayheroinfraquerydsl.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class QueryDslConfiguration {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
---
+++ onedayhero-infra-querydsl/src/main/java/com/sixheroes/onedayheroinfraquerydsl/missionrequest/dto/MissionRequestQueryDto.java
@@ -0,0 +1,26 @@
+package com.sixheroes.onedayheroinfraquerydsl.missionrequest.dto;
+
+import com.sixheroes.onedayherodomain.mission.MissionCategoryCode;
+import com.sixheroes.onedayherodomain.mission.MissionStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public record MissionRequestQueryDto(
+    Long missionRequestId,
+    Long missionId,
+    MissionStatus missionStatus,
+    Integer bookmarkCount,
+    LocalDateTime missionCreatedAt,
+    LocalDate missionDate,
+    LocalTime startTime,
+    LocalTime endTime,
+    Integer price,
+    MissionCategoryCode missionCategoryCode,
+    String categoryName,
+    String si,
+    String gu,
+    String dong
+) {
+}
