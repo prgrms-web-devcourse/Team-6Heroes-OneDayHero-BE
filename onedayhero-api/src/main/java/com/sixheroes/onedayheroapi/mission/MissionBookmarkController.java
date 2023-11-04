269ea19c76ad8941b6f364ab0f430a7c050364f5
PHJ <legowww@gmail.com>
2023-11-05 03:47:26 +0900

    [SIX-141] feat & test : 미션 찜 조회 api 모듈 컨트롤러 및 테스트 작성

--- onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/mission/MissionBookmarkController.java
+++ onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/mission/MissionBookmarkController.java
@@ -6,8 +6,11 @@ import com.sixheroes.onedayheroapi.mission.request.MissionBookmarkCreateRequest;
 import com.sixheroes.onedayheroapplication.mission.MissionBookmarkService;
 import com.sixheroes.onedayheroapplication.mission.response.MissionBookmarkCancelResponse;
 import com.sixheroes.onedayheroapplication.mission.response.MissionBookmarkCreateResponse;
+import com.sixheroes.onedayheroapplication.mission.response.MissionBookmarkMeViewResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,12 +23,23 @@ public class MissionBookmarkController {
 
     private final MissionBookmarkService missionBookmarkService;
 
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<MissionBookmarkMeViewResponse>> me(
+            @PageableDefault(size = 3) Pageable pageable,
+            Long userId //TODO: @Auth Long userId
+    ) {
+        var viewResponse = missionBookmarkService.me(
+                pageable,
+                userId
+        );
+        return ResponseEntity.ok().body(ApiResponse.ok(viewResponse));
+    }
+
     @PostMapping
     public ResponseEntity<ApiResponse<MissionBookmarkCreateResponse>> createMissionBookmark(
             @Valid @RequestBody MissionBookmarkCreateRequest request
     ) {
         var response = missionBookmarkService.createMissionBookmark(request.toService());
-
         return ResponseEntity.created(URI.create("/api/v1/missions/" + response.missionId())).body(ApiResponse.created(response));
     }
 
@@ -34,8 +48,6 @@ public class MissionBookmarkController {
             @Valid @RequestBody MissionBookmarkCancelRequest request
     ) {
         var response = missionBookmarkService.cancelMissionBookmark(request.toService());
-
         return ResponseEntity.ok().body(ApiResponse.ok(response));
     }
-
 }
