66c168c4db2ec078946245b5b3383b88ddcf210d
heenahan <mby1997s@gmail.com>
2023-11-06 00:39:26 +0900

    [SIX-126] fix : sonar 설정 수정

--- onedayhero-application/src/test/java/com/sixheroes/onedayheroapplication/mission/MissionServiceTest.java
+++ onedayhero-application/src/test/java/com/sixheroes/onedayheroapplication/mission/MissionServiceTest.java
@@ -11,6 +11,7 @@ import com.sixheroes.onedayherodomain.mission.repository.MissionBookmarkReposito
 import com.sixheroes.onedayherodomain.mission.repository.MissionCategoryRepository;
 import com.sixheroes.onedayherodomain.mission.repository.MissionRepository;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -27,6 +28,7 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Disabled
 class MissionServiceTest extends IntegrationApplicationTest {
 
     @Autowired
