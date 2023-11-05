434b97b032bd0401ac06ab0be22178a7e1ed4302
heenahan <mby1997s@gmail.com>
2023-11-06 02:16:36 +0900

    [SIX-126] fix : sonar cloud 설정 추가

--- onedayhero-application/src/test/java/com/sixheroes/onedayheroapplication/mission/MissionServiceTest.java
+++ onedayhero-application/src/test/java/com/sixheroes/onedayheroapplication/mission/MissionServiceTest.java
@@ -11,7 +11,6 @@ import com.sixheroes.onedayherodomain.mission.repository.MissionBookmarkReposito
 import com.sixheroes.onedayherodomain.mission.repository.MissionCategoryRepository;
 import com.sixheroes.onedayherodomain.mission.repository.MissionRepository;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -28,7 +27,6 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@Disabled
 class MissionServiceTest extends IntegrationApplicationTest {
 
     @Autowired
