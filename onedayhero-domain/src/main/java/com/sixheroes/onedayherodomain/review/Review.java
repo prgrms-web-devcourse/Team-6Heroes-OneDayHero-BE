760a1b6fcb95f665ff9ab0cd96c83a83b6db697c
PHJ <legowww@gmail.com>
2023-11-13 00:40:55 +0900

    [SIX-182] feat : 리뷰 Soft Delete 적용

--- onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/review/Review.java
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/review/Review.java
@@ -1,15 +1,20 @@
 package com.sixheroes.onedayherodomain.review;
 
+import com.sixheroes.onedayherodomain.global.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE reviews SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted = false")
 @Table(name = "reviews")
 @Entity
-public class Review {
+public class Review extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,4 +37,7 @@ public class Review {
 
     @Column(name = "content", length = 500, nullable = false)
     private String content;
+
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted;
 }
