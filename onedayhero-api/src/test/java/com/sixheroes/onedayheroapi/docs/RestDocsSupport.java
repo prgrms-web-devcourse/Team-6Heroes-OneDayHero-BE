9f1d0136163623d450f06cc08ad07fc19ed5e0a7
PHJ <legowww@gmail.com>
2023-11-13 18:24:48 +0900

    [SIX-184] feat : 테스트 환경에서 @AuthUser 유저 식별 기능 추가 및 일부 적용

--- onedayhero-api/src/test/java/com/sixheroes/onedayheroapi/docs/RestDocsSupport.java
+++ onedayhero-api/src/test/java/com/sixheroes/onedayheroapi/docs/RestDocsSupport.java
@@ -3,9 +3,15 @@ package com.sixheroes.onedayheroapi.docs;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import com.sixheroes.onedayheroapi.global.argumentsresolver.authuser.AuthUserArgumentResolver;
 import com.sixheroes.onedayheroapi.global.handler.GlobalExceptionHandler;
+import com.sixheroes.onedayheroapi.global.interceptor.JwtAuthInterceptor;
+import com.sixheroes.onedayheroapplication.global.jwt.JwtProperties;
+import com.sixheroes.onedayheroapplication.global.jwt.JwtTokenManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
@@ -17,8 +23,13 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
 @ExtendWith(RestDocumentationExtension.class)
+@Import(JwtTestConfiguration.class)
 public abstract class RestDocsSupport {
 
+    @Autowired
+    private JwtProperties jwtProperties;
+    @Autowired
+    private JwtTokenManager jwtTokenManager;
     protected MockMvc mockMvc;
     protected ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
@@ -31,9 +42,18 @@ public abstract class RestDocsSupport {
                         .operationPreprocessors()
                         .withRequestDefaults(prettyPrint())
                         .withResponseDefaults(prettyPrint()))
-                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver(), new AuthUserArgumentResolver(jwtProperties))
+                // @AuthUser Long userId 적용된 컨트롤러만 인터셉터 임시 적용
+                .addMappedInterceptors(new String[]{
+                        "/api/v1/me/reviews/*",
+                        "/api/v1/me/bookmarks"
+                }, new JwtAuthInterceptor(jwtProperties, jwtTokenManager))
                 .build();
     }
 
     protected abstract Object setController();
+
+    protected String getAccessToken() {
+        return "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwicm9sZSI6Ik1FTUJFUiIsImlhdCI6MTY5OTg2MTcxNCwiZXhwIjoxNjk5OTIxNzE0fQ.v_xYMcOc4WIno8rIgUTfyCRjyT1vQrsMGcZVrLASyEI";
+    }
 }
