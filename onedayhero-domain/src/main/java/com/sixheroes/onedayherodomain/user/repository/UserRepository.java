33cea8d1d5579ad0743a924808d51bf6c5e654e6
heenahan <mby1997s@gmail.com>
2023-11-14 13:11:47 +0900

    [SIX-155] fix : 리베이스 머지하면서 잘못 충돌 해결한 부분 수정

--- onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/user/repository/UserRepository.java
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/user/repository/UserRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-}
 
+    Optional<User> findByEmail_Email(String email_email);
+}
