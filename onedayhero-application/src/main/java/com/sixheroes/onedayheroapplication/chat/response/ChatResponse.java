b215c787b31504b871d941421e48e0e2ade3df33
Bombo <bombo96@chungwoon.ac.kr>
2023-11-15 15:05:23 +0900

    [SIX-174] fix : 충돌 해결

--- onedayhero-application/src/main/java/com/sixheroes/onedayheroapplication/chat/response/ChatResponse.java
+++
@@ -1,9 +0,0 @@
-package com.sixheroes.onedayheroapplication.chat.response;
-
-public record ChatResponse(
-        Long id,
-        Long chatRoomId,
-        String message,
-        String chatType
-) {
-}
