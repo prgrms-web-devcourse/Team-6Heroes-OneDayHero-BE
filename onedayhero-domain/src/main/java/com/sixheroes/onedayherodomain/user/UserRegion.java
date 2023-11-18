d8e22933db35e33d11352f67956fd5407727e6f3
heenahan <mby1997s@gmail.com>
2023-11-18 14:30:50 +0900

    [SIX-221] feat : 단방향에서 OnDeleted cascade 옵션 사용

--- onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/user/UserRegion.java
+++ onedayhero-domain/src/main/java/com/sixheroes/onedayherodomain/user/UserRegion.java
@@ -6,6 +6,8 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -23,6 +25,7 @@ public class UserRegion extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 
     @Column(name = "region_id", nullable = false)
