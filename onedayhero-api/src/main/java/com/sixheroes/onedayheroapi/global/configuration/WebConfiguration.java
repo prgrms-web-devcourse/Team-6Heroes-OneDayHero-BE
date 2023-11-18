a52c87ce4fd61b60cd1ada60b4f281bf11a73b7e
PHJ <legowww@gmail.com>
2023-11-18 20:14:36 +0900

    [SIX-FIX] CORS 설정 복귀

--- onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/global/configuration/WebConfiguration.java
+++ onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/global/configuration/WebConfiguration.java
@@ -10,7 +10,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:80", "http://localhost:3000")
+                .allowedOrigins("http://localhost:80")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
