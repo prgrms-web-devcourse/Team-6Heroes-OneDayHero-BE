7fb728957a0fc3dce7b4eea083c049998135b6d8
PHJ <legowww@gmail.com>
2023-11-21 02:55:19 +0900

    [SIX-234] feat : 로그인 실패 확인 로직 작성

--- onedayhero-application/src/main/java/com/sixheroes/onedayheroapplication/oauth/infra/feign/decoder/LoginErrorDecoder.java
+++ onedayhero-application/src/main/java/com/sixheroes/onedayheroapplication/oauth/infra/feign/decoder/LoginErrorDecoder.java
@@ -1,23 +1,23 @@
 package com.sixheroes.onedayheroapplication.oauth.infra.feign.decoder;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sixheroes.onedayherocommon.error.ErrorCode;
+import feign.FeignException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
 @NoArgsConstructor
 public class LoginErrorDecoder implements ErrorDecoder {
 
-    private final static ObjectMapper objectMapper = new ObjectMapper();
     private final ErrorDecoder errorDecoder = new Default();
 
     @Override
     public Exception decode(String methodKey, Response response) {
-        log.warn("로그인 과정에서 에러가 발생했습니다.");
+        var feignException = FeignException.errorStatus(methodKey, response);
+        var errorMessage = feignException.getMessage();
+        log.warn("로그인 과정에서 에러가 발생했습니다. {}", errorMessage);
 
         return switch (response.status()) {
             case 400, 401, 500 -> new IllegalStateException(ErrorCode.L_001.name());
