3a08b4653f3ddb9b5b514bdce5033d950bfa921f
heenahan <mby1997s@gmail.com>
2023-11-23 18:09:25 +0900

    [SIX-252] feat & test : 제안할 미션 조회 응답값에 지역과 미션 정보 추가 및 테스트

--- onedayhero-application/src/main/java/com/sixheroes/onedayheroapplication/mission/repository/response/MissionMatchingQueryResponse.java
+++ onedayhero-application/src/main/java/com/sixheroes/onedayheroapplication/mission/repository/response/MissionMatchingQueryResponse.java
@@ -1,35 +1,29 @@
 package com.sixheroes.onedayheroapplication.mission.repository.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sixheroes.onedayherodomain.mission.MissionCategoryCode;
 import com.sixheroes.onedayherodomain.mission.MissionStatus;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public record MissionMatchingQueryResponse(
     Long id,
-
     String title,
-
-    Long categoryId,
-
-    MissionCategoryCode categoryCode,
-
-    String categoryName,
-
-    String si,
-
-    String gu,
-
-    String dong,
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    Integer price,
     LocalDate missionDate,
-
+    LocalTime startTime,
+    LocalTime endTime,
+    LocalDateTime missionCreatedAt,
+    Long categoryId,
+    MissionCategoryCode categoryCode,
+    String categoryName,
+    Long regionId,
+    String si,
+    String gu,
+    String dong,
     Integer bookmarkCount,
-
     MissionStatus missionStatus,
-
     Long bookmarkId
 ) {
 }
