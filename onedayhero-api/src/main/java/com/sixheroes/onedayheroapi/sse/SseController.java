3172c8975e360dfc829b68a058e31fac49571ac9
heenahan <mby1997s@gmail.com>
2023-11-27 21:57:37 +0900

    [SIX-HOTFIX] refactor : 이벤트가 아닌 메시지로 응답

--- onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/sse/SseController.java
+++ onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/sse/SseController.java
@@ -1,15 +1,20 @@
 package com.sixheroes.onedayheroapi.sse;
 
 import com.sixheroes.onedayheroapi.global.argumentsresolver.authuser.AuthUser;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/api/v1/sse")
 @RestController
 public class SseController {
@@ -19,11 +24,22 @@ public class SseController {
     private final SseEmitters sseEmitters;
 
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public ResponseEntity<SseEmitter> subscribe(
+    public SseEmitter subscribe(
+        HttpServletRequest httpServletRequest,
+        HttpServletResponse httpServletResponse,
         @AuthUser Long userId
     ) {
         var sseEmitter = sseEmitters.add(userId);
+        try {
+            sseEmitter.send("sse emitter 응답 보내기");
+            log.info("sse emitter controller에서 응답 보내영");
+        } catch (IOException e) {
+            log.error("보내는데 오류났어요!");
+            throw new RuntimeException(e);
+        }
+        log.info("request : {}", httpServletRequest);
+        log.info("response : {}", httpServletResponse);
         sseEmitters.send(userId, DUMMY_DATA_NAME, DUMMY_DATA);
-        return ResponseEntity.ok(sseEmitter);
+        return sseEmitter;
     }
 }
