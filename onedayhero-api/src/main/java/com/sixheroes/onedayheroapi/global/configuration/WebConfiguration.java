540bac35ffdad52603ddf509f7cccd9922a49385
Bombo <bombo96@chungwoon.ac.kr>
2023-11-29 19:08:19 +0900

    [SIX-HOTFIX] fix : cors 설정 변경

--- onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/global/configuration/WebConfiguration.java
+++ onedayhero-api/src/main/java/com/sixheroes/onedayheroapi/global/configuration/WebConfiguration.java
@@ -15,7 +15,7 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000")
                 .allowedOrigins("https://onedayhero.kro.kr")
                 .allowedOrigins("https://team-6-heroes-one-day-hero-fe-git-develop-howon-shin.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
                 .allowCredentials(true)
