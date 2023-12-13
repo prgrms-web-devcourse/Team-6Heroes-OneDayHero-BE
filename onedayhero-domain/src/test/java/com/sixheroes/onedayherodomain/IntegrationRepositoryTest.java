0d190639d9ce39534a4c2a453bb6a55f1bd3b7a0
heenahan <mby1997s@gmail.com>
2023-12-13 16:22:46 +0900

    [SIX-277] test : 스크립트 테스트를 위한 실패 테스트 작성

--- onedayhero-domain/src/test/java/com/sixheroes/onedayherodomain/IntegrationRepositoryTest.java
+++ onedayhero-domain/src/test/java/com/sixheroes/onedayherodomain/IntegrationRepositoryTest.java
@@ -1,9 +1,17 @@
 package com.sixheroes.onedayherodomain;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @SpringBootTest
 public abstract class IntegrationRepositoryTest {
+
+    @Test
+    void failTest() {
+        assertThat(2 + 3).isEqualTo(2);
+    }
 }
