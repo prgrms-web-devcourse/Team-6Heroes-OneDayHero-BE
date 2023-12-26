b43845822a1c1a71799e56c298f8b86d3c7bc2b2
heenahan <mby1997s@gmail.com>
2023-12-27 00:04:44 +0900

    [SIX-126] fix : 실패한 테스트 삭제

--- onedayhero-domain/src/test/java/com/sixheroes/onedayherodomain/IntegrationRepositoryTest.java
+++ onedayhero-domain/src/test/java/com/sixheroes/onedayherodomain/IntegrationRepositoryTest.java
@@ -1,17 +1,9 @@
 package com.sixheroes.onedayherodomain;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @SpringBootTest
 public abstract class IntegrationRepositoryTest {
-
-    @Test
-    void failTest() {
-        assertThat(2 + 3).isEqualTo(2);
-    }
 }
